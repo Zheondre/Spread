48d815ad80861f6908459b1b4cbe38ea84957587
zheondre <zheondre@gmail.com>
2019-11-21 20:31:59 -0500

    adding in bullet logic this needs to be tested

--- libgdx/android/src/com/mygdx/entities/objects/bullet.java
+++ libgdx/android/src/com/mygdx/entities/objects/bullet.java
@@ -1,22 +1,27 @@
 package com.mygdx.entities.objects;
 
+import com.badlogic.gdx.ai.msg.Telegram;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.entities.entity;
 import com.mygdx.entities.entityInfo;
+import com.mygdx.entities.humans.player;
 import com.mygdx.world.gameMap;
 
 import static com.mygdx.utils.entUtils.getBullet;
+import static com.mygdx.utils.entUtils.getMoveUpVec;
+import static java.lang.StrictMath.abs;
 
 public class bullet extends entity {
 
-    public static final int Speed = 500;
+    public static final int vSpeed = 100;
     public boolean remove;
-
 
    public bullet(entityInfo eT, gameMap map, float x, float y){
         super(eT,map);
-        setImage(getBullet()); //Make this Static
+        //setImage(getBullet()); //Make this Static
+       setImage(map.getPlayerHealth()); // temp
     }
 
     public void update(float dt){
@@ -29,7 +34,7 @@ public class bullet extends entity {
         if(getPosX() > mMap.getMapWidth() || getPosX() < 0)
             remove = true;
 
-        /*
+        /*if we have a collision delete
             apply force in the direction the bullet was fired in
 
          */
@@ -42,7 +47,6 @@ public class bullet extends entity {
 
     public void render(SpriteBatch batch){
        //shoot requested draw image
-
         Texture image = getImage();
         if(image != null)
             batch.draw(image,mPos.x, mPos.y, getWidth(), getHeight());
@@ -52,7 +56,35 @@ public class bullet extends entity {
        return true;
     }
 
-    public void shoot(entity shooter){
+    public void shoot(entity target){
+
+        float tangle;
+        float vy;
+        float vx;
+
+        if(target != null) {
+            getEntDistance(target);
+            float tempx = target.getPosX() - this.mPos.x;
+            float tempy = target.getPosY() - this.mPos.y;
+            tangle = (float)Math.atan(tempy/tempx);
+           vy =  (float)(vSpeed * Math.sin(tangle));
+           vx = (float) (vSpeed * Math.cos(tangle));
+             Vector2 tv = new Vector2(vx,vy); //this might slow things down, change later
+            getBody().applyLinearImpulse(tv, getBody().getWorldCenter(), true);
+        }
+    }
+    public boolean handleMessage(Telegram msg) {
+        return true;
+    }
+    public void shoot(Vector2 vPos){
+
+    }
+
+    public void shootDebug(player play){
+
+//play.getHost().
+
+
 
     }
 }
